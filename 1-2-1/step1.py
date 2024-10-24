29a93c689208f5137779bff1315b6e4536c4083a
dylan <dylan.noah@vtsd.com>
2024-10-24 12:25:53 -0400

    1-2-1 day 1

---
+++ 1-2-1/step1.py
@@ -0,0 +1,19 @@
+# a121_catch_a_turtle.py
+#-----import statements-----
+import turtle as trtl
+
+#-----game configuration----
+spot_color = "pink"
+spot_size = 2
+spot_shape = "circle"
+#-----initialize turtle-----
+spot = trtl.Turtle()
+spot_shapesize(spot_size)
+spot_color(spot_color)
+#-----game functions--------
+
+
+#-----events----------------
+
+wn = trtl.Screen()
+wn.mainloop()
