230e826a788c2729c3bc212be4f046a4366c3bcf
15020 <dylan.noah@vtsd.com>
2024-10-30 12:27:17 -0400

    1-2-1

--- 1-2-1/step1.py
+++ 1-2-1/step1.py
@@ -2,20 +2,34 @@
 #-----import statements-----
 import turtle as trtl
 
+import random as rand
+
+
 #-----game configuration----
 spot_color = "pink"
 spot_size = 2
 spot_shape = "circle"
+score = 0
+clicks = 1
 #-----initialize turtle-----
 spot = trtl.Turtle()
-spot.shapesize(spot_size)
 spot.color(spot_color)
 spot.fillcolor(spot_color)
+spot.shape(spot_shape)
+spot.shapesize(spot_size)
+
 #-----game functions--------
+spot.penup()
+def change_position():
+    new_xpos = rand.randint(-400, 400)
+    new_ypos = rand.randint(-300, 300)
+    spot.goto(new_xpos,new_ypos)
 def spot_clicked( x, y):
-    print("Hello World!")
+   change_position()
+update_score
+print(score)
+
 #-----events----------------
 spot.onclick(spot_clicked)
 wn = trtl.Screen()
-
 wn.mainloop()
