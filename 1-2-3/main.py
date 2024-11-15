9c7c749ee2219384a06dc030944903f7dc7cabd7
15020 <dylan.noah@vtsd.com>
2024-11-15 13:11:06 -0500

    1-2

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -3,11 +3,13 @@ import turtle as trtl
 
 #-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
-
+ground_height = -150
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
+drawer = trtl.Turtle()
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
-
 apple = trtl.Turtle()
 
 #-----functions-----
@@ -16,13 +18,19 @@ def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
-def apple_down(active_apple):
-  x_cor = active_apple.xcor()
-  y_cor = active_apple.ycor()
-  active_apple.goto(x_cor, y_cor - 300)
+def drop_apple():
+  apple.goto(apple.xcor(), ground_height)
+
+def draw_an_a():
+  drawer.color("white")
+  drawer.write("A", font=("Arial", 74, "bold"))
 
 #-----function calls-----
+drawer.hideturtle()
+apple.penup()
 draw_apple(apple)
-apple_down(apple)
+draw_an_a()
+wn.onkeypress(drop_apple, "a")
+wn.listen()
 wn.bgpic("background.gif")
-wn.mainloop()
+trtl.mainloop()
